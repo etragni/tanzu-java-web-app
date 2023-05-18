f1bb0028f02c23b5986976ad1a72d666ef2ef635
Eknathreddy09 <eknath.reddy09@gmail.com>
2023-05-18 10:19:48 +0530

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot, Welcome to TAP Session hosted for Chitale Dairy";
+		return "Greetings from Spring Boot, Welcome to TAP Session hosted for Chitale Dairy and team";
 	}
 
 }
